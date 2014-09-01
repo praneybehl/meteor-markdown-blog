cfe5249787f643193e8074b842faf99096bbf0d6
Jeremy Shimko <jshimko@cloudlinemedia.co>
2014-08-31 23:10:45 -0400

    remove scss package and use Compass CLI instead

---
+++ client/scss/config.rb
@@ -0,0 +1,11 @@
+require 'susy'
+require 'breakpoint'
+require 'compass-notify'
+
+css_dir = "../css"
+sass_dir = "."
+
+output_style = :expanded
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
