6e3afbc9e804334d68b518c72540625d76facb25
Jeremy Shimko <jshimko@cloudlinemedia.co>
2014-09-14 23:46:35 -0400

    implement basic post inserting

---
+++ client/views/posts/post_editor.js
@@ -0,0 +1,4 @@
+
+Template.PostEditor.rendered = function () {
+    $('body').addClass('editor');  // required for Ghost layout styles to work properly
+};
---
+++ client/views/posts/posts_new.js
@@ -0,0 +1,29 @@
+
+Template.PostsNew.events({
+    'click .button-save': function (e, t) {
+        e.preventDefault();
+
+        var post = {
+            title: $('#entry-title').val(),
+            markdown: Session.get('editorValue'),
+            wordCount: Session.get('wordCount')
+        };
+
+        // console.log(post);
+        if(post){
+            if(!post.title){
+                throw new Meteor.Error(602, 'Please fill in a title');
+            }
+            if(!post.markdown){
+                throw new Meteor.Error(603, 'Please write some post content!');
+            }
+            var id = Posts.insert(post);
+            Router.go('Posts', {_id: id});
+        }
+    }
+});
+
+
+Template.PostsNew.rendered = function () {
+
+};
